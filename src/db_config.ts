7ab100b1c8776c6bfcd74f0487a3e39cfada5ecc
Owen Lee <owen0004@e.ntu.edu.sg>
2023-08-30 13:14:38 +0800

    fix: hopefully fixed now

--- src/db_config.ts
+++ src/db_config.ts
@@ -3,17 +3,33 @@ import { Link } from "./entities/links";
 require("dotenv").config();
 
 const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
+  // type: "postgres",
+  // url: process.env.DATABASE_URL,
   // host: process.env.DATABASE_HOST,
   // port: +process.env.DATABASE_PORT,
   // username: process.env.DATABASE_USER,
   // password: process.env.DATABASE_PASSWORD,
   // database: process.env.DATABASE_NAME,
+  // entities: [Link],
+  // synchronize: true,
+  // logging: true,
+  // ssl: true,
+
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+
+  // Load SSL cert provided by Render
+  extra: {
+    sslConnectionOptions: {
+      rejectUnauthorized: false,
+      sslmode: "require",
+    },
+  },
+
   entities: [Link],
   synchronize: true,
   logging: true,
-  ssl: true,
+
   // type: "postgres",
   // host: "localhost",
   // port: 5435,
